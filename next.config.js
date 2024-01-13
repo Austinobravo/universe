f9df00847341155ae49402930092c4549be0f2a8
Austinobravo <Austinobravo@gmail.com>
2024-01-13 20:38:48 +0100

    pushed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: [
+//             'assets.coingecko.com'
+//         ]
+//     }
+// }
+
+// module.exports = nextConfig
 const nextConfig = {
     images: {
-        domains: [
-            'assets.coingecko.com'
-        ]
-    }
-}
-
-module.exports = nextConfig
+      domains: ['assets.coingecko.com'],
+    },
+    experimental: {
+      reactRoot: true, // This enables React Strict Mode
+      optimizeCss: true,
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=60, s-maxage=60, stale-while-revalidate=60',
+            },
+          ],
+        },
+      ];
+    },
+    // Add any other configurations you need...
+  };
+  
+  module.exports = nextConfig;
+  
